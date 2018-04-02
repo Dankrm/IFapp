bcaa3d1d7e2e6a65fbe0a683942b3ed2978d9843
Dankrm <danton.mohr@gmail.com>
2018-04-02 14:16:00 -0300

    5

---
+++ app/src/main/java/com/example/aluno/ifapp/ContextoDados.java
@@ -0,0 +1,25 @@
+package com.example.aluno.ifapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+//
+//public class ContextoDados extends SQLiteOpenHelper {
+//    private static final String NOME_BD = "Ifapp";
+//    private static final int VERSAO_BD = 1;
+//    private static final String LOG_TAG = "Ifapp";
+//    private final Context contexto;
+//
+//    public ContextoDados(Context context) {
+//        super(context, NOME_BD, null, VERSAO_BD);
+//        this.contexto = context;
+//    }
+//
+//    @Override
+//    public void onCreate(SQLiteDatabase db)
+//    {
+//
+//
+//
+//    }
+//}
